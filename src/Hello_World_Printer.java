fd0cd7548b68eb9fe477cc23765a0cc9df553a57
montalex <alexis.montavon@epfl.ch>
2016-02-27 13:15:47 +0100

    clean!

--- src/Hello_World_Printer.java
+++ src/Hello_World_Printer.java
@@ -7,9 +7,7 @@ public class Hello_World_Printer {
     public static void main(String[] args) {
        System.out.println("Hello World");
        //Also blablablablablabalb
-<<<<<<< HEAD
        System.out.println("I Love Maria");
->>>>>>> Alexis_branch
 
     }
 
