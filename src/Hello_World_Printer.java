52a2e5a7d0eaecd3ebcef1f683fbcd2d9fe48e7a
montalex <alexis.montavon@epfl.ch>
2016-02-27 13:13:16 +0100

    love

--- src/Hello_World_Printer.java
+++ src/Hello_World_Printer.java
@@ -7,6 +7,7 @@ public class Hello_World_Printer {
     public static void main(String[] args) {
        System.out.println("Hello World");
        //Also blablablablablabalb
+       System.out.println("I Love Maria");
 
     }
 
